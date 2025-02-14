b0577b1d55ff8ee836481c75a942b5dca2c8fb87
bellac <bellac@rightmove.co.uk>
2025-02-14 15:58:02 +0000

    updating styling

--- src/utils/consultationQuestions.ts
+++ src/utils/consultationQuestions.ts
@@ -1,7 +1,7 @@
 export const questions = [
-    "Do you have a known allergy to Genovian Pears?",
+    "Do you consent to the collection of your personal information?",
     "Have you experienced an allergic reaction in the past 6 months?",
     "Are you currently taking any other medication?",
     "Do you have a history of severe anaphylaxis?",
-    "Would you like to proceed with the consultation?"
+    "Would you like to proceed with the consultation and submit your answers to a professional?"
 ];
